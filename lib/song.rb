a45d941536c8535ee8b13195b418107c55e82da0
Damon R <dmarcr1997@gmail.com>
2020-01-31 02:33:53 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -12,6 +12,19 @@ class Song
     return new_song
   end
   
+   def self.create_by_name(name)
+    new_song = self.new
+    new_song.name = name
+    @@all << new_song
+    return new_song
+  end
+  
+  def self.new_by_name(name)
+     new_song = self.create
+     new_song.name = name
+     new_song
+  end
+   
   def self.find_by_name(name)
     @@all.each do |song|
       if song.name == name
@@ -30,11 +43,6 @@ class Song
     end
   end
   
-  def self.alphabetical
-    sorted= []
-    sorted = @@all.sort_by{|a| a.name}
-    sorted
-  end
   
   def self.new_from_filename(filename)
     new_song = self.create
@@ -52,23 +60,18 @@ class Song
     @@all << new_song
   end
   
+  def self.alphabetical
+    sorted= []
+    sorted = @@all.sort_by{|a| a.name}
+    sorted
+  end
+  
   def self.destroy_all
     @@all = []
   end
+  
   def save
     self.class.all << self
-  end
-  
-  def self.create_by_name(name)
-    new_song = self.new
-    new_song.name = name
-    @@all << new_song
-    return new_song
-  end
-  def self.new_by_name(name)
-     new_song = self.create
-     new_song.name = name
-     new_song
   end
 end
 song = Song.create
