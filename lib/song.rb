4b25ee7aa6ff19615e5f7fef8bc8679a7983c47b
cpach96 <cpach96@gmail.com>
2020-02-27 12:11:10 -0500

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,6 +1,41 @@
+require 'pry'
 class Song
   attr_accessor :name, :artist_name
   @@all = []
+
+  def self.create()
+    song = self.new
+    @@all << song
+    song
+  end
+
+  def self.new_by_name(name)
+    song = self.new
+    song.name = name
+    song
+  end
+
+  def self.create_by_name(name)
+    song = self.new
+    song.name = name
+    @@all << song
+    song
+  end
+
+  def self.find_by_name(name)
+    @@all.find{|n| n.name == name}
+  end
+
+  def self.find_or_create_by_name(name)
+    if  self.find_by_name(name)
+
+      self.find_by_name(name)
+       
+    else
+      self.create_by_name(name)
+
+    end
+end
 
   def self.all
     @@all
@@ -10,4 +45,33 @@ class Song
     self.class.all << self
   end
 
+  def self.alphabetical
+    @@all.sort_by{|song| song.name }
+      
+  end
+
+  def self.new_from_filename(filename)
+    song = self.new
+    
+    song.name = filename.split(/[-.]/)[1].strip
+    song.artist_name = filename.split(/[-.]/)[0].strip
+    song
+    
+  end
+
+  
+  def self.create_from_filename(filename)
+    song = self.new
+    
+    song.name = filename.split(/[-.]/)[1].strip
+    song.artist_name = filename.split(/[-.]/)[0].strip
+    song.save
+    song
+    
+  end
+
+  def self.destroy_all
+    @@all.clear
+
+  end
 end
